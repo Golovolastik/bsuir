665199ef09f005cd3c78d1fbbe571821e9bf4927
Golovolastik <72813121+Golovolastik@users.noreply.github.com>
2021-10-24 20:26:30 +0300

    Update main.py
    bissextile algh and some alerts

--- fse/ipt_1/main.py
+++ fse/ipt_1/main.py
@@ -21,13 +21,38 @@ def weekday(y: int, m: int, d: int) -> int:
 
 # вывод дня недели
 def clicked():
-	year_value = int(year.get())
-	month_value = month.get()
-	month_value = month_dict[f"{month_value}"]
-	year_value = int(year.get())
-	day_value = int(day.get())
-
-	messagebox.showinfo('Title', weekday(year_value, month_value, day_value))
+	try:
+		month_value = month.get()
+		month_value = month_dict[f"{month_value}"]
+		day_value = int(day.get())
+		# проверка правильности введенного дня
+		if (day_value < 1 or day_value > 31):
+			message_1 = "Выберите существующий день"
+			messagebox.showinfo('Ошибка', message_1)
+			return
+		year_value = int(year.get())
+		if year_value < 1582:
+			message_2 = 'Выберите дату не раньше 15.10.1582. \
+						 Именно в тот день был введен григорианский календарь'
+			messagebox.showinfo('Ошибка', message_2)
+			return		
+		year_value = int(year.get())
+		# проверка на февраль и високосность
+		if year_value % 400 == 0:
+			bissextile = True
+		elif year_value % 100 == 0:
+			bissextile = False
+		elif year_value % 4 == 0:
+			bissextile = True
+		else:
+			bissextile = False
+		if (month_value == 2 and bissextile == False and day_value > 28):
+			messagebox.showinfo('Ошибка', 'В феврале 28 дней')
+			return
+	except:
+		messagebox.showinfo('Ошибка', 'Введите дату')
+		return
+	messagebox.showinfo('День недели', weekday(year_value, month_value, day_value))
 
 
 # создание основного окна
@@ -42,13 +67,15 @@ window.title("Вычисление дня недели по дате")
 text_1 = "Выберите дату"
 lbl_1 = Label(window, text=text_1)
 lbl_1.place(x=115, y=5)
+#text_2 = "Этот год \nвисокосный"
+#lbl_2 = Label(window, text=text_2)
+#lbl_2.place(x=250, y=5)
 
 # создание поля для выбора дня
 day_list = [x for x in range(1, 32)]
 day = Combobox(window, values=day_list, width=3)
 day.current(14)
 day.place(x=40, y=50)
-#day_value = int(day.get())
 
 # создание поля для выбора месяца
 month_dict = {'Январь': 1, 'Февраль': 2, 'Март': 3,
@@ -58,22 +85,22 @@ month_dict = {'Январь': 1, 'Февраль': 2, 'Март': 3,
 month = Combobox(window, values=list(month_dict.keys()), state="readonly", width=10)
 month.current(9)
 month.place(x=110, y=50)
-#month_value = month.get()
-#month_value = month_dict[f"{month_value}"]
 
 # создание поля для ввода года
-default = StringVar(window, value=1582)
-year = Entry(window, textvariable=default, width=7)
-year.place(x=250, y=47)
-#year_value = int(year.get())
+#default = StringVar(window, value=1582)
+#year = Entry(window, textvariable=default, width=7)
+year = Spinbox(window, from_=1582, to=2100, width=7, bg='gray')
+year.place(x=250, y=50)
 
 # Кнопка запуска алгоритма
 btn = Button(window, text='Узнать', command=clicked, width=10)
 btn.grid(column=1, row=0)
 btn.place(x=105, y=100)
 
-
-#print(weekday(year_value, month_value, day_value))
-
+year_value = int(year.get())
+if year_value == 1583:
+	text_2 = "Этот год \nвисокосный"
+	lbl_2 = Label(window, text=text_2)
+	lbl_2.place(x=250, y=5)
 
 window.mainloop()
