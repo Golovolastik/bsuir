30d07c8515521fdc88b3f705ad2a9b91745f77eb
Golovolastik <72813121+Golovolastik@users.noreply.github.com>
2021-10-24 17:12:33 +0300

    add button

--- fse/ipt_1/main.py
+++ fse/ipt_1/main.py
@@ -1,6 +1,25 @@
 # импорт библиотек
 from tkinter import *
 from tkinter.ttk import Combobox
+from tkinter import messagebox
+
+
+# основные функции
+
+# алгоритм для подсчета дня недели по дате
+def weekday(y: int, m: int, d: int) -> int:
+	if m < 3:
+		y -= 1
+		m += 10
+	else:
+		m -= 2
+	return (d + 31 * m // 12 + y + y // 4 - y // 100 + y // 400) % 7
+
+
+# вывод дня недели
+def clicked():
+	messagebox.showinfo('Title', 'Text')
+
 
 # создание основного окна
 window = Tk()
@@ -12,44 +31,36 @@ window.title("Вычисление дня недели по дате")
 
 # текст в окне программы
 text_1 = "Выберите дату"
-lbl = Label(window, text=text_1)
-#lbl.grid(column=0, row=0)
-lbl.place(x=90, y=5)
+lbl_1 = Label(window, text=text_1)
+lbl_1.place(x=90, y=5)
 
 # создание поля для выбора дня
 day_list = [x for x in range(1, 32)]
 day = Combobox(window, values=day_list, width=3)
 day.current(14)
 day.place(x=40, y=50)
+#day_value = day.get()
 
 # создание поля для выбора месяца
-month_list = ['Январь', 'Февраль', 'Март',
-			  'Апрель', 'Май', 'Июнь',
-			  'Июль', 'Август', 'Сентябрь',
-			  'Октябрь', 'Ноябрь', 'Декабрь']
-month = Combobox(window, values=month_list, state="readonly", width=10)
+month_dict = {'Январь': 1, 'Февраль': 2, 'Март': 3,
+			  'Апрель': 4, 'Май': 5, 'Июнь': 6,
+			  'Июль': 7, 'Август': 8, 'Сентябрь': 9,
+			  'Октябрь': 10, 'Ноябрь': 11, 'Декабрь': 12}
+month = Combobox(window, values=month_dict.keys, state="readonly", width=10)
 month.current(6)
 month.place(x=110, y=50)
+#month_value = month.get()
 
 # создание поля для ввода года
-year_list = [x for x in range(2010, 2031)]
-#year = Combobox(window, values=year_list, width=7)
-#year.place(x=250, y=50)
-#year.current(0)
+#default = StringVar(window, value=1410)
+#year = Entry(window, textvariable=default, width=7)
+#year.place(x=250, y=47)
+#year_value = year.get()
 
-default = StringVar(window, value=1410)
-year = Entry(window, textvariable=default, width=7)
-year.place(x=250, y=47)
+# Кнопка запуска алгоритма
+btn = Button(window, text='Узнать', command=clicked)
+btn.grid(column=1, row=0)
+btn.place(x=30, y=100)
+
 
 window.mainloop()
-
-
-def weekday(year: int, month: int, day: int) -> int:
-	if month < 3:
-		year -= 1
-		month += 10
-	else:
-		month -= 2
-	return (day + 31 * month // 12 + year + year // 4 - year // 100 + year // 400) % 7
-
-# print(weekday(1991, 12, 10))
