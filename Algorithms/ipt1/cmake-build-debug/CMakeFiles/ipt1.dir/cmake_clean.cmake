2a8a4a1e5182bc63ef0deb3e22d25f0e6b61cac8
Golovolastik <yankovich.aliaksei@gmail.com>
2022-09-09 12:43:19 +0300

    hash function

---
+++ Algorithms/ipt1/cmake-build-debug/CMakeFiles/ipt1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ipt1.dir/main.cpp.o"
+  "ipt1"
+  "ipt1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ipt1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Algorithms/ipt1/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+struct LIST{
+    string value;
+    LIST* next;
+};
+
+void add(LIST* dict, string element);
+int hash_function(string element);
+
+int main() {
+    LIST* dict[255];
+    while (1){
+        cout << "1) Add" << endl;
+        cout << "2) Search" << endl;
+        cout << "3) Delete" << endl;
+        cout << "4) Info" << endl;
+        cout << "5) Exit" << endl;
+        int i;
+        cin >> i;
+        switch (i) {
+            case 1: {
+                cout << "Enter element:" << endl;
+                string element;
+                cin >> element;
+                add(*dict, element);
+                break;
+            }
+            case 2: {
+                cout << "Hello" << endl;
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+            case 5: {
+                return 0;
+            }
+        }
+    }
+}
+
+void add(LIST* dict, string element){
+    int hash = hash_function(element);
+    LIST temp = dict[hash];
+    return;
+}
+
+int hash_function(string element){
+    int result = 0;
+    for (int i=0; i<element.length(); i++){
+        result += element[i];
+    }
+    result %= 255;
+    cout << "Hash = " << result << endl;
+    return result;
+}
